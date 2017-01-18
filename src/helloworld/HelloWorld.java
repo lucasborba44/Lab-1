bbebab0398d06baac5fa5806c2a9a09f881773cb
Lucas <Lucas@10.250.19.142>
2017-01-18 14:46:26 -0500

    Added card and deck of cards classes

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+
+package helloworld;
+
+/**
+ *
+ * @author Lucas
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World, my GitHub account name is lucasborba44");
+    }
+    
+}
