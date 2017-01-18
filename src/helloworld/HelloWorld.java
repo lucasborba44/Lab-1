6c42ab5d63ce8709d190224e3e6a513b0fa6d98c
Lucas <Lucas@10.250.19.142>
2017-01-18 14:50:00 -0500

    Lab1

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -6,9 +6,7 @@ package helloworld;
  */
 public class HelloWorld {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         System.out.println("Hello World, my GitHub account name is lucasborba44");
     }
